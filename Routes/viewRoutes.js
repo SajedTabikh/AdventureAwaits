4207e11d843d99c59b46c8803a12723e68ddeba3
SajedTabikh <sajedtabikh07@gmail.com>
2023-08-04 19:35:44 +0300

    Updated

---
+++ Routes/viewRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const viewsController = require('./../controllers/viewsController');
+const authController = require('./../controllers/authController');
+
+router.use(viewsController.alerts);
+
+router.get(
+  '/All-Tours',
+  authController.protect,
+  authController.isLoggedIn,
+  viewsController.getOverview
+);
+router.get('/', viewsController.getPerview);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/signup', viewsController.getSignUpForm);
+router.get('/me', authController.protect, viewsController.getAccount);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
+
+router.post('/submit-user-data', authController.protect, viewsController.updateUserData);
+
+module.exports = router;
