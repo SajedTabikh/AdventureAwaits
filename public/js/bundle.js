d3fcf87dd81028084bf2499535e6859f792dc989
SajedTabikh <sajedtabikh07@gmail.com>
2023-08-12 20:19:19 +0300

    Updated

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12605,7 +12605,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "9758" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "5214" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
